100f8ef0c9fefc6b166db6ce439ecbe983a4067c
Stefan Papp <stefan_papp@web.de>
2010-09-27 14:51:24 +0200

    WPF Calculator added

---
+++ WPFCalculator/WPFCalculator/MyTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFCalculator
+{
+    class MyTextBox : System.Windows.Controls.TextBox
+    {
+        protected override void OnPreviewGotKeyboardFocus(System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            e.Handled = true;
+            base.OnPreviewGotKeyboardFocus(e);
+        }
+ 
+    }
+}
